f61a786fa214ba54673bd0722715f0d64a7dcbeb
“Ruairidh <2250079m@student.gla.ac.uk>
2018-12-25 21:39:57 +0000

    Adding user authentication to the REST API

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,10 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework.reverse import reverse
 from rest_framework.decorators import api_view
+from rest_framework import permissions
+from django.contrib.auth.models import User
 
 @api_view
 def api_root(request, format=None):
@@ -10,11 +12,23 @@ def api_root(request, format=None):
         'snippets': reverse('snippet-list', request=request, format=format)
     })
 
-
 class SnippetList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
+
+
