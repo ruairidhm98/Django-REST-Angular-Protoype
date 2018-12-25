8b8a3f67ea1d39de7012c5cf4e512c14350e13dc
“Ruairidh <2250079m@student.gla.ac.uk>
2018-12-25 22:03:37 +0000

    USER authentication working, going to start work on the front end now

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,11 +6,11 @@ class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner',)
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('id', 'username', 'snippets',)
 
--- snippets/views.py
+++ snippets/views.py
@@ -5,6 +5,7 @@ from rest_framework.reverse import reverse
 from rest_framework.decorators import api_view
 from rest_framework import permissions
 from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
 
 @api_view
 def api_root(request, format=None):
@@ -13,7 +14,7 @@ def api_root(request, format=None):
     })
 
 class SnippetList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
@@ -21,7 +22,8 @@ class SnippetList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, 
+                          IsOwnerOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
