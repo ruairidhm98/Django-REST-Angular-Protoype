125211710b652b7625879042e36682af15cb6aae
“Ruairidh <2250079m@student.gla.ac.uk>
2018-12-25 23:00:53 +0000

    Some stuff made on angular the now

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FunComponent } from './home/fun/fun.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FunComponent,
+    PageTwoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-app/src/app/home/fun/fun.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fun',
+  templateUrl: './fun.component.html',
+  styleUrls: ['./fun.component.css']
+})
+export class FunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+
+}
