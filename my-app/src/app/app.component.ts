6939a318fe5284ff83d963529a6a4fdd51aa349e
“Ruairidh <2250079m@student.gla.ac.uk>
2018-12-26 15:00:34 +0000

    Added all html files, need to make them specific to each CRUD op now

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css', 
+    './vendor/bootstrap/css/bootstrap.min.css',
+    './vendor/fontawesome-free/css/all.min.css',
+    './vendor/datatables/dataTables.bootstrap4.css',
+    './css/sb-admin.css',
+  ]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
 }
-
 
