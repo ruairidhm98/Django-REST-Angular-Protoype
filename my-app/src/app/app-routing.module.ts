d61c7e8d4669d1f22c804f6614a45e4adecf6d5b
“Ruairidh <2250079m@student.gla.ac.uk>
2018-12-26 12:55:24 +0000

    Angular tutorial complete, going to use the REST api I made to see if it works

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'search', component: HeroSearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App!';
+  title = 'Tour of Heroes';
 }
 
 
