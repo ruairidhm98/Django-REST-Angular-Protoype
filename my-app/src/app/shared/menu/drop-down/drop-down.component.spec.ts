485e5c6fbf1bf17b9a9514dd8766a27030ae79d9
“Ruairidh <2250079m@student.gla.ac.uk>
2019-01-09 12:04:29 +0000

    Forgot to remove one

--- my-app/src/app/shared/menu/drop-down/drop-down.component.spec.ts
+++ my-app/src/app/shared/menu/drop-down/drop-down.component.spec.ts
@@ -5,21 +5,4 @@ import { DropDownComponent } from './drop-down.component';
 describe('DropDownComponent', () => {
   let component: DropDownComponent;
   let fixture: ComponentFixture<DropDownComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DropDownComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DropDownComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
