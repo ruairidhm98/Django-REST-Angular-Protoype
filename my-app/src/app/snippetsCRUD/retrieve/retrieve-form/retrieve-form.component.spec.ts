56ab06deace77c9d716a2175abad2e181958e132
“Ruairidh <2250079m@student.gla.ac.uk>
2019-01-09 12:11:59 +0000

    Removed all tests

--- my-app/src/app/snippetsCRUD/retrieve/retrieve-form/retrieve-form.component.spec.ts
+++ my-app/src/app/snippetsCRUD/retrieve/retrieve-form/retrieve-form.component.spec.ts
@@ -1,25 +1,3 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RetrieveFormComponent } from './retrieve-form.component';
-
-describe('RetrieveFormComponent', () => {
-  let component: RetrieveFormComponent;
-  let fixture: ComponentFixture<RetrieveFormComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RetrieveFormComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RetrieveFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
